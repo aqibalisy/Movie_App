38a0a691c3881e2686d0f37ec52b1646249eed4a
Aqib Ali <101803091+aqibalisy@users.noreply.github.com>
2022-09-04 23:50:19 +0530

    Checkpoint 4
    Checkpoint 4

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,13 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import Index from './index';
+
+
+test('renders learn react link', () => {
+  const { getByText } = render(<Index/>);
+}); 
